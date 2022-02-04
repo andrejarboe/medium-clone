516667d99eaeda7611157a7048fc0388aaff15c6
Andre Jarboe <andrejarboe@gmail.com>
2022-02-04 15:19:24 -0500

    post loop on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
+import { log } from 'console'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../lib/sanity'
+import { Post } from '../lib/typings'
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
+
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
 
       {/* Banner */}
@@ -34,9 +45,60 @@ export default function Home() {
       </div>
       {/* END Banner */}
 
-      {/* posts */}
-        
-      {/* end posts */}
+      {/* posts SSR */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                src={urlFor(post.mainImage).url()!}
+                alt={post.title}
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+              />
+              <div className="flex justify-between items-center bg-white p-5 ">
+                <div className="">
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <div className="h-24 w-24 flex items-center justify-end">
+                  <img
+                    src={urlFor(post.author.image).url()!}
+                    className="h-12 w-12 rounded-full object-cover"
+                    alt={post.author.name}
+                  />
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+
+      {/* end posts SSR */}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == 'post']{
+  _id,
+  title,
+  author-> {
+  name,
+  image
+  },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
