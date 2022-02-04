0709c862c6a193b9de947421db942988698585f5
Andre Jarboe <andrejarboe@gmail.com>
2022-02-04 12:38:28 -0500

    cursor-pointer

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="mx-auto flex max-w-7xl justify-between p-5">
+    <header className="mx-auto flex max-w-7xl justify-between p-5 ">
       {/* left  */}
       <div className="flex items-center space-x-5">
         <Link href="/">
@@ -13,9 +13,9 @@ export default function Header() {
           />
         </Link>
         <div className="hidden items-center space-x-5 md:inline-flex">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+          <h3 className="cursor-pointer">About</h3>
+          <h3 className="cursor-pointer">Contact</h3>
+          <h3 className="cursor-pointer rounded-full bg-green-600 px-4 py-1 text-white">
             Follow
           </h3>
         </div>
@@ -23,8 +23,10 @@ export default function Header() {
       {/* end left  */}
       {/* right  */}
       <div className="flex items-center space-x-5 text-green-600 ">
-        <h3 className="">Sign In</h3>
-        <h3 className="rounded-full border px-4 py-1">Get Started</h3>
+        <h3 className="cursor-pointer">Sign In</h3>
+        <h3 className="cursor-pointer rounded-full border px-4 py-1">
+          Get Started
+        </h3>
       </div>
       {/* end right  */}
     </header>
