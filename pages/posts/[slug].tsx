47fdb6d215722be19e4469a79e3c512b7667d761
Andre Jarboe <andrejarboe@gmail.com>
2022-02-04 16:55:48 -0500

    add titel, description, and author

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -8,18 +8,38 @@ interface Props {
 }
 
 function Posts({ post }: Props) {
-//   console.log('***********page info*************')
+  //   console.log('***********page info*************')
 
-//   console.log(post)
+  //   console.log(post)
 
   return (
     <main>
       <Navbar />
 
-      <img 
-        className='w-full h-40 object-cover object-center'
-        src={urlFor(post.mainImage).url()!} 
-      alt={post.title} />
+      <img
+        className="h-40 w-full object-cover object-center"
+        src={urlFor(post.mainImage).url()!}
+        alt={post.title}
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+          />
+          <p className="text-sm font-extralight">
+            Blog Post by <span className="text-green-600">{post.author.name} </span> -
+            Published at {new Date(post._createdAt).toLocaleDateString()}
+          </p>
+        </div>
+      </article>
     </main>
   )
 }
