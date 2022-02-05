8d3e7d524a03ab2d62f832938b56bb067df5c10e
Andre Jarboe <andrejarboe@gmail.com>
2022-02-04 20:07:58 -0500

    npm i @sanity/client

--- pages/api/createComments.ts
+++ pages/api/createComments.ts
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import sanityClient from '@sanity/client'
 
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_TOKEN,
+}
+
+const client = sanityClient(config)
 
 export default function createComment(
   req: NextApiRequest,
